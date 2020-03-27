9ab7493855c9059b1adcab0113429e75f54cd70c
DiegoRSM03 <rodrigo.dsanchez@yahoo.com.ar>
2020-03-27 02:17:12 -0300

    Inicio de Proyecto
    El proyecto consta de una aplicacion web que cumple el rol de
    una libreria virtual, con usuarios, prestamos, libros y es un
    CRUD basicamente.
    La idea del proyecto va a ser utilizar:
    -JAVASCRIPT(JQUERY, AJAX)
    -PHP
    -MYSQL(PDO)
    -Patron de diseño MVC

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Biblioteca DRSM</title>
+</head>
+<body>
+	
+
+	<script src="view/assets/js/jquery.js"></script>
+	<script src="view/assets/js/app.js"></script>
+</body>
+</html>
