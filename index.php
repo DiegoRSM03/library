2b0ee3eafaf9ba857b835bfd863377916bd5f3f7
DiegoRSM03 <rodrigo.dsanchez@yahoo.com.ar>
2020-03-28 01:08:26 -0300

    /view/assets/fonts/ modificada
    *Elimine fuentes que no utilizaré en el proyecto para
    no consumir espacio de mas simplemente.

--- index.php
+++ index.php
@@ -3,18 +3,22 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="./view/assets/css/style.css">
+	<link rel="stylesheet" href="./view/assets/iconfonts/flaticon.css">
+
 	<title>Biblioteca DRSM</title>
 </head>
 <body>
 	<header>
 		<h1>Biblioteca DRSM</h1>
-		<div id="nav">
-			<a href="">Home</a>
-			<a href="">Acerca</a>
-			<a href="">Iniciar Sesión</a>
-			<a href="">Registrarse</a>
-			<a href="">Contacto</a>
-		</div>
+		<nav>
+			<button type="submit">Inicio</button>
+			<button type="submit">Acerca</button>
+			<button type="submit">Iniciar Sesión</button>
+			<button type="submit">Registrarse</button>
+			<button type="submit">Contacto</button>
+		</nav>
 	</header>
 
 	<script src="view/assets/js/app.js"></script>
